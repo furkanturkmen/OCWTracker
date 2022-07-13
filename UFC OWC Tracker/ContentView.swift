293c26dbec0094c9f77c7acce87e2e97c1d97cac
Furkan Turkmen <the.furkan@icloud.com>
2022-07-13 02:09:50 +0200

    Initial Commit

---
+++ UFC OWC Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UFC OWC Tracker
+//
+//  Created by Furkan Türkmen on 13/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UFC OWC Tracker/UFC_OWC_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UFC_OWC_TrackerApp.swift
+//  UFC OWC Tracker
+//
+//  Created by Furkan Türkmen on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct UFC_OWC_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
